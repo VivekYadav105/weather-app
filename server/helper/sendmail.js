7520f44c8e1b2ba062812e0d2f2a7b8e0308a0c7
P.Vivek Yadav <82657104+VivekYadav105@users.noreply.github.com>
2022-09-29 00:20:59 +0530

    authentication stage 1 done

---
+++ server/helper/sendmail.js
@@ -0,0 +1,10 @@
+const jwt = require('jsonwebtoken')
+const nodeMailer = require('node-mailer')
+const sendMail = async (task,data,mailid)=>{
+    
+    var message = task==verify?"Click on the url below for vrtification.\n(!!!!use same browser you used before!!!!)":"Click on the url to reset Password.\n(!!!!use same browser you used before!!!!)"     
+    
+}
+
+
+
