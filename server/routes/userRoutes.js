3523d95ac31dc53b51aaa99d9d64c3ecc9819d5c
Vivek <20bcs105@iiitdwd.ac.in>
2024-06-16 16:48:01 +0530

    vercel - cors - 2

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,12 +1,18 @@
 const {Router} = require('express');
 
-const {loginUser,signupUser} = require('../controllers/userController')
+const {loginUser,signupUser} = require('../controllers/userController');
+const { userModel } = require('../models/user');
 
 const userRouter = Router();
 
 userRouter.post('/login',loginUser);
 
 userRouter.get('/',(req,res)=>{res.send('hello from user')})
+
+userRouter.get('/users',(req,res,next)=>{
+    const users = userModel.find({})
+    res.send(users)
+})
 
 userRouter.post('/signup',signupUser);
 
